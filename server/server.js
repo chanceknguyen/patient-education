9a9e15c73b5d2855856698003cd99220d3f7c9d8
Chance Nguyen <chanceknguyen@gmail.com>
2021-10-02 16:04:44 -0500

    port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 const server = app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 
